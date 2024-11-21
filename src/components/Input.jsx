4bbd8d50d41ee42f296f95d00d13158b42d7e6ea
seetaramoruganti <orugantiseetaram@gmail.com>
2024-11-21 18:37:15 -0500

    added styles to inputs, buttons,

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,17 @@
 export default function Input({ label, textarea, props }) {
   return (
-    <p>
-      <label>{label}</label>
-      {textarea ? <textarea {...props} /> : <input {...props} />}
+    <p className="flex flex-col gap-1 my-4">
+      <label className="text-sm font-bold uppercase text-stone-500">
+        {label}
+      </label>
+      {textarea ? (
+        <textarea
+          className="w-full p-1 border-b-2 rounded-sm border-stone-300"
+          {...props}
+        />
+      ) : (
+        <input {...props} />
+      )}
     </p>
   );
 }
--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -9,7 +9,7 @@ export default function NewTask() {
           </button>
         </li>
         <li>
-          <button className=" px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950">
+          <button className="rounded px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950">
             Save
           </button>
         </li>
