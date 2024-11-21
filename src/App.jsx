33629e012a1f258ca9b6d77a6df8633ef401afc0
seetaramoruganti <orugantiseetaram@gmail.com>
2024-11-21 18:32:39 -0500

    added components - input, task, sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import NewTask from "./components/NewTask";
+import ProjectsSidebar from "./components/ProjectsSidebar";
+
 function App() {
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <main className="h-screen my-8 flex gap-8">
+        <ProjectsSidebar />
+        <NewTask />
+      </main>
     </>
   );
 }
